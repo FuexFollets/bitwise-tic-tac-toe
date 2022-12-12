29e7329a71b9931ef9e54bd2bebfd5f39e87dbfb
Fuexfollets <fuex9follets@gmail.com>
2022-12-12 18:15:02 +0000

    Added Win check function

--- src/bitboard.hpp
+++ src/bitboard.hpp
@@ -3,11 +3,23 @@
 #include <iostream>
 #include <cstdint>
 #include <exception>
+#include <array>
 
 namespace tictactoe {
     struct bitboard {
         enum turn_values {
-            x = 'X', y = 'Y'
+            x = 'X', y = 'Y', none = static_cast<char>(0)
+        };
+
+        const static std::array<std::uint16_t, 8> bitwise_win_checks {
+            0x111'000'000,
+            0x000'111'000,
+            0x000'000'111,
+            0x100'100'100,
+            0x010'010'010,
+            0x001'001'001,
+            0x100'010'001,
+            0x001'010'100
         };
 
         struct not_valid_move : std::exception {};
@@ -43,6 +55,23 @@ namespace tictactoe {
                     player_y |= bit_position;
                     break;
             }
+        }
+
+        char winner() {
+            int player_number {}; // 0 = X, 1 = O;
+            for (const auto player : {player_x, player_y}) {
+                for (const auto check_mask : bitwise_win_checks) {
+                    if (player & check_mask == player) {
+                        return (player_number == 0) ?
+                            (turn_values::x) :
+                            (turn_values::y);
+                    }
+                }
+
+                player_number++;
+            }
+
+            return turn_values::none;
         }
 
         std::ostream& operator<<(std::ostream&, const bitboard&);
